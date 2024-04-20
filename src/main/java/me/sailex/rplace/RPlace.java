5ca6b6d2cceab76e93caa866cfb8c1368a6915ec
Elias Hahn <elias.hahn@mail.de>
2024-04-20 10:34:09 +0200

    initial commit

---
+++ src/main/java/me/sailex/rplace/RPlace.java
@@ -0,0 +1,17 @@
+package me.sailex.rplace;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class RPlace extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
